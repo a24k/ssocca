39e0effb5bbe9c775e6a10ccac02acf98c0baed0
Atsushi KAWASAKI <ak@xlix.org>
2023-01-20 17:23:41 +0900

    add --headless option.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(about, author, version)]
+struct Args {
+    /// Use browser in headless mode
+    #[arg(long, default_value_t = false)]
+    headless: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:?}", args);
 }
