6768241a2fdd3c11f912a1e8108b2c22b2eddd08
Atsushi KAWASAKI <ak@xlix.org>
2023-01-28 11:36:05 +0900

    add timeout option to cli args.

--- src/args.rs
+++ src/args.rs
@@ -11,8 +11,12 @@ pub struct Args {
     pub verbosity: clap_verbosity_flag::Verbosity,
 
     /// Specify path to a Chrome executable
-    #[arg(long)]
+    #[arg(long, value_name = "PATH")]
     pub chrome: Option<std::path::PathBuf>,
+
+    /// Timeout duration in secs
+    #[arg(long, default_value_t = 10, value_name = "SEC")]
+    pub timeout: u8,
 
     /// Url to initiate authentication
     pub url: String,
@@ -62,5 +66,19 @@ mod tests {
     fn chrome(#[case] expected: Option<PathBuf>, #[case] input: Vec<&str>) {
         let args = Args::parse_from(input);
         assert_eq!(expected, args.chrome);
+    }
+
+    #[rstest]
+    #[case(
+        10,
+        vec!["ssocca", "https://example.com/"],
+    )]
+    #[case(
+        5,
+        vec!["ssocca", "--timeout", "5", "https://example.com/"],
+    )]
+    fn timeout(#[case] expected: u8, #[case] input: Vec<&str>) {
+        let args = Args::parse_from(input);
+        assert_eq!(expected, args.timeout);
     }
 }
