7ce7802bbee6c9bfd71b3ee99c3776f923d40866
Atsushi KAWASAKI <ak@xlix.org>
2023-02-04 20:48:35 +0900

    handle all Results with logger.

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,43 @@ mod acquirer;
 mod args;
 mod logger;
 
+use anyhow::anyhow;
+use async_std::task;
+use std::process::ExitCode;
+
 use acquirer::{Acquirer, AcquirerConfig};
 use args::Args;
+use log::error;
 
-#[async_std::main]
-async fn main() -> anyhow::Result<()> {
-    let args: Args = clap::Parser::parse();
+fn main() -> ExitCode {
+    async fn main(args: &Args) -> anyhow::Result<()> {
+        let acquirer = Acquirer::launch(AcquirerConfig::build(args)?).await?;
 
-    logger::init(&args.verbosity);
+        acquirer.dump().await?;
+        acquirer.navigate(&args.url).await?;
+        acquirer.dump().await?;
 
-    let acquirer = Acquirer::launch(AcquirerConfig::build(&args)?).await?;
+        acquirer.close().await
+    }
 
-    acquirer.dump().await?;
-    acquirer.navigate(&args.url).await?;
-    acquirer.dump().await?;
+    let args: anyhow::Result<Args> = clap::Parser::try_parse().map_err(|e| anyhow!(e));
 
-    acquirer.close().await
+    match args {
+        Ok(args) => {
+            logger::init(&args.verbosity);
+            let ret = task::block_on(async { main(&args).await });
+            match ret {
+                Ok(()) => ExitCode::SUCCESS,
+                Err(e) => {
+                    error!("{}", e);
+                    ExitCode::FAILURE
+                }
+            }
+        }
+        Err(e) => {
+            env_logger::builder().init();
+            error!("{}", e);
+            ExitCode::FAILURE
+        }
+    }
 }
