99912cda361561cc055ac69d55659855f94776e3
Atsushi KAWASAKI <ak@xlix.org>
2023-02-11 05:04:27 +0900

    add rule-finish.

--- src/acquirer/scenario.rs
+++ src/acquirer/scenario.rs
@@ -9,11 +9,21 @@ pub mod rule {
     #[allow(dead_code)]
     pub enum Rule {
         Start(Start),
+        Finish(Finish),
     }
 
     #[allow(dead_code)]
     pub struct Start {
         pub goto: NavigateParams,
+    }
+
+    type CookieKey = String;
+    type CookieDomain = String;
+
+    #[allow(dead_code)]
+    pub struct Finish {
+        pub on: Option<CookieDomain>,
+        pub with: Vec<CookieKey>,
     }
 }
 
@@ -21,7 +31,7 @@ pub mod rule {
 mod tests {
     use rstest::*;
 
-    use super::rule::Start;
+    use super::rule::*;
 
     #[rstest]
     #[case(
@@ -34,5 +44,30 @@ mod tests {
         assert_eq!(None, input.goto.transition_type);
         assert_eq!(None, input.goto.frame_id);
         assert_eq!(None, input.goto.referrer_policy);
+    }
+
+    #[rstest]
+    #[case(
+        vec!["cookey"],
+        None,
+        Finish { with: vec!["cookey".into()], on: None },
+    )]
+    #[case(
+        vec!["cookey1", "cookey2"],
+        None,
+        Finish { with: vec!["cookey1".into(), "cookey2".into()], on: None },
+    )]
+    #[case(
+        vec!["cookey1", "cookey2"],
+        Some("example.com"),
+        Finish { with: vec!["cookey1".into(), "cookey2".into()], on: Some("example.com".into()) },
+    )]
+    fn finish(
+        #[case] expected_with: Vec<&str>,
+        #[case] expected_on: Option<&str>,
+        #[case] input: Finish,
+    ) {
+        assert_eq!(expected_with, input.with);
+        assert_eq!(expected_on.map(|str| str.into()), input.on);
     }
 }
