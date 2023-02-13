e9ffbc72293379298714536953c2244c7727a6f0
Atsushi KAWASAKI <ak@xlix.org>
2023-02-14 00:17:43 +0900

    urlpattern is too complecated.

--- src/acquirer/scenario.rs
+++ src/acquirer/scenario.rs
@@ -6,8 +6,6 @@ pub struct Scenario {
 pub mod rule {
     use chromiumoxide::cdp::browser_protocol::page::NavigateParams;
 
-    use urlpattern::UrlPattern;
-
     #[allow(dead_code)]
     pub enum Rule {
         Start(Start),
@@ -20,6 +18,7 @@ pub mod rule {
         pub goto: NavigateParams,
     }
 
+    type UrlPattern = String;
     type CssSelector = String;
 
     #[allow(dead_code)]
@@ -44,9 +43,6 @@ mod tests {
     use rstest::*;
 
     use super::rule::*;
-    use regex::Regex;
-    use url::Url;
-    use urlpattern::{UrlPattern, UrlPatternInit, UrlPatternMatchInput};
 
     #[rstest]
     #[case(
@@ -59,58 +55,6 @@ mod tests {
         assert_eq!(None, rule.goto.transition_type);
         assert_eq!(None, rule.goto.frame_id);
         assert_eq!(None, rule.goto.referrer_policy);
-    }
-
-    #[rstest]
-    #[case(
-        "https://example.com",
-        "input[type=email]",
-        "mail@example.com",
-        Input { on: None, to: "input[type=email]".into(), value: "mail@example.com".into() },
-        )]
-    #[case(
-        "https://example.com",
-        "input[type=email]",
-        "mail@example.com",
-        Input {
-            on: Some(UrlPattern::parse(UrlPatternInit::parse_constructor_string::<Regex>("https://*.com", None).unwrap()).unwrap()),
-            to: "input[type=email]".into(), value: "mail@example.com".into() },
-        )]
-    #[case(
-        "https://example.com/login/first?auth=id",
-        "input[type=email]",
-        "mail@example.com",
-        Input {
-            on: Some(UrlPattern::parse(UrlPatternInit::parse_constructor_string::<Regex>("https://*.com/login/first?auth=:auth", None).unwrap()).unwrap()),
-            to: "input[type=email]".into(), value: "mail@example.com".into() },
-        )]
-    #[case(
-        "https://example.com/login/first?auth=id",
-        "input[type=email]",
-        "mail@example.com",
-        Input {
-            on: Some(UrlPattern::parse(UrlPatternInit{
-                hostname: Some("*.com".into()),
-                pathname: Some("/login/:path".into()),
-                search: Some("auth=:auth".into()),
-                ..Default::default()
-            }).unwrap()),
-            to: "input[type=email]".into(), value: "mail@example.com".into() },
-        )]
-    fn input(
-        #[case] match_url: &str,
-        #[case] expected_to: &str,
-        #[case] expected_value: &str,
-        #[case] rule: Input,
-    ) {
-        match rule.on {
-            Some(pattern) => assert!(pattern
-                .test(UrlPatternMatchInput::Url(Url::parse(match_url).unwrap()))
-                .unwrap()),
-            None => assert!(true),
-        }
-        assert_eq!(expected_to, rule.to);
-        assert_eq!(expected_value, rule.value);
     }
 
     #[rstest]
