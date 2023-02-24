b7f462e270a24a4a1b45b12782676cba0114b2e9
Atsushi KAWASAKI <ak@xlix.org>
2023-02-25 05:54:31 +0900

    refactor rules to separate file.

--- src/acquirer/scenario.rs
+++ src/acquirer/scenario.rs
@@ -1,3 +1,5 @@
+pub mod rule;
+
 use anyhow::anyhow;
 use async_std::fs;
 use serde::{Deserialize, Serialize};
@@ -71,53 +73,6 @@ impl Scenario {
     }
 }
 
-pub mod rule {
-    use chromiumoxide::cdp::browser_protocol::page::NavigateParams;
-    use serde::{Deserialize, Serialize};
-
-    #[derive(Clone, Debug, Deserialize, Serialize)]
-    #[serde(tag = "type", rename_all = "lowercase")]
-    pub enum Rule {
-        Input(Input),
-        Totp(Totp),
-        Click(Click),
-    }
-
-    #[derive(Clone, Debug, Deserialize, Serialize)]
-    pub struct Start(pub NavigateParams);
-
-    type UrlPattern = String;
-    type CssSelector = String;
-
-    #[derive(Clone, Debug, Deserialize, Serialize)]
-    pub struct Input {
-        pub on: Option<UrlPattern>,
-        pub to: CssSelector,
-        pub value: String,
-    }
-
-    #[derive(Clone, Debug, Deserialize, Serialize)]
-    pub struct Totp {
-        pub on: Option<UrlPattern>,
-        pub to: CssSelector,
-        pub seed: String,
-    }
-
-    #[derive(Clone, Debug, Deserialize, Serialize)]
-    pub struct Click {
-        pub on: Option<UrlPattern>,
-        pub to: CssSelector,
-    }
-
-    type CookieKey = String;
-    type CookieDomain = String;
-
-    #[derive(Clone, Debug, Deserialize, Serialize)]
-    pub struct Finish {
-        pub on: Option<CookieDomain>,
-        pub with: Vec<CookieKey>,
-    }
-}
 
 #[cfg(test)]
 mod tests {
@@ -136,30 +91,5 @@ mod tests {
         assert_eq!(None, rule.0.transition_type);
         assert_eq!(None, rule.0.frame_id);
         assert_eq!(None, rule.0.referrer_policy);
-    }
-
-    #[rstest]
-    #[case(
-        vec!["cookey"],
-        None,
-        Finish { with: vec!["cookey".into()], on: None },
-    )]
-    #[case(
-        vec!["cookey1", "cookey2"],
-        None,
-        Finish { with: vec!["cookey1".into(), "cookey2".into()], on: None },
-    )]
-    #[case(
-        vec!["cookey1", "cookey2"],
-        Some("example.com"),
-        Finish { with: vec!["cookey1".into(), "cookey2".into()], on: Some("example.com".into()) },
-    )]
-    fn finish(
-        #[case] expected_with: Vec<&str>,
-        #[case] expected_on: Option<&str>,
-        #[case] rule: Finish,
-    ) {
-        assert_eq!(expected_with, rule.with);
-        assert_eq!(expected_on.map(|str| str.into()), rule.on);
     }
 }
