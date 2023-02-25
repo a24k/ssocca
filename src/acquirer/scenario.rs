098f7ee04bc144c8d1b7799ff384b468d8cbdc64
Atsushi KAWASAKI <ak@xlix.org>
2023-02-26 05:43:33 +0900

    test overrides.

--- src/acquirer/scenario.rs
+++ src/acquirer/scenario.rs
@@ -34,15 +34,15 @@ impl Scenario {
         scenario: anyhow::Result<Scenario>,
         args: &Args,
     ) -> anyhow::Result<Scenario> {
-        let start = scenario.as_ref().map_or_else(
-            |error| match &args.url {
-                Some(url) => Ok(rule::Start(url.into())),
-                None => {
+        let start = match &args.url {
+            Some(url) => Ok(rule::Start(url.into())),
+            None => scenario.as_ref().map_or_else(
+                |error| {
                     Err(anyhow!("{error}")).context("Found no toml configuration or url option.")
-                }
-            },
-            |scenario| Ok(scenario.start.clone()),
-        )?;
+                },
+                |scenario| Ok(scenario.start.clone()),
+            ),
+        }?;
 
         let rules = scenario
             .as_ref()
@@ -82,6 +82,7 @@ mod tests {
     use rstest::*;
 
     use super::*;
+    use crate::{args, args::Args};
 
     #[rstest]
     #[case(
@@ -91,7 +92,7 @@ mod tests {
             finish: rule::Finish { on: None, with: vec![] },
         },
         r#"[start]
-           url = "https://example.com""#
+           url = "https://example.com""#,
     )]
     #[case(
         Scenario {
@@ -127,9 +128,64 @@ mod tests {
            value = "value02"
 
            [finish]
-           with = ["cookey01", "cookey02"]"#
+           with = ["cookey01", "cookey02"]"#,
     )]
     fn build_from_toml(#[case] expected: Scenario, #[case] toml: String) {
         assert_eq!(expected, Scenario::build_from_toml(toml).unwrap());
+    }
+
+    #[rstest]
+    #[case(
+        Scenario {
+            start: rule::Start("https://example.org".into()),
+            rules: vec![],
+            finish: rule::Finish { on: None, with: vec![] },
+        },
+        r#"[start]
+           url = "https://example.com""#,
+        args!("--url", "https://example.org"),
+    )]
+    #[case(
+        Scenario {
+            start: rule::Start("https://example.org".into()),
+            rules: vec![
+                rule::Rule::Input(rule::Input {
+                    on: None,
+                    to: "selector01".into(),
+                    value: "value01".into(),
+                }),
+                rule::Rule::Input(rule::Input {
+                    on: None,
+                    to: "selector02".into(),
+                    value: "value02".into(),
+                }),
+            ],
+            finish: rule::Finish {
+                on: None,
+                with: vec!["cookey01".into(), "cookey02".into(), "cookey03".into()]
+            },
+        },
+        r#"[start]
+           url = "https://example.com"
+
+           [[rules]]
+           type = "input"
+           to = "selector01"
+           value = "value01"
+
+           [[rules]]
+           type = "input"
+           to = "selector02"
+           value = "value02"
+
+           [finish]
+           with = ["cookey01", "cookey02"]"#,
+        args!("--url", "https://example.org", "--cookie", "cookey03"),
+    )]
+    fn override_with_args(#[case] expected: Scenario, #[case] toml: String, #[case] args: Args) {
+        assert_eq!(
+            expected,
+            Scenario::override_with_args(Scenario::build_from_toml(toml), &args).unwrap()
+        );
     }
 }
